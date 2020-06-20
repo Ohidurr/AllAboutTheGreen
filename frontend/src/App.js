26c43ac6b9eb68d66bbbf7a422062242b8df107d
darsuabasi <abasibasiurua@gmail.com>
2020-06-20 13:37:48 -0400

    test on new branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Router } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Submissions from './components/Submissions'
 import OpenForum from './components/OpenForum'
 
@@ -16,8 +16,7 @@ const App = () => {
           <OpenForum/>
         </Route>
 
-        
-
+      
       </Switch>
     </div>
   );
