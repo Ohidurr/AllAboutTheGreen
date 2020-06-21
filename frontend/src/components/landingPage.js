726004138b1938fa7780b3d6c7c98bc73803e095
Henrynunez112 <henrynunez@pursuit.org>
2020-06-21 16:38:00 -0400

    updated plantContainer

--- frontend/src/components/landingPage.js
+++ frontend/src/components/landingPage.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react"
 import axios from 'axios'
-import PlantPage from './PlantPage'
+// import PlantPage from './PlantPage'
 import '../css/landingPage.css'
 
 const { REACT_APP_API } = process.env;
